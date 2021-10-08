e24c34e887e6061233041cc99e91f37e7a3e281c
ron harel <rontsohrl@gmail.com>
2021-10-08 13:31:20 +0300

    Initial commit

---
+++ units/unit2/client_scream.py
@@ -0,0 +1,16 @@
+import socket
+# The param AF_INET indicates that we are using IP protocol
+# The param SOCKET_STREAM defines using TCP protocol
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+my_socket.connect(("127.0.0.1", 8820))
+data = ""
+while data != "Bye":
+    msg = input("Please enter your message\n:")
+    # encode translate the str to binary
+    my_socket.send(msg.encode())
+    # the param in the recv method defines the max bytes we want to extract from the socket
+    # decode method translate the binary sequence to string
+    data = my_socket.recv(1024).decode()
+    print("The server sent: " + data)
+
+my_socket.close()
