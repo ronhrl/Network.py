913ef1a6d6d021c345a79e25336bac4d4477578e
ron harel <rontsohrl@gmail.com>
2021-10-11 11:11:18 +0300

    UDP

---
+++ units/unit3-UDP/client_udp.py
@@ -0,0 +1,14 @@
+import socket
+
+SERVER_IP = "127.0.0.1"
+PORT = 8821
+MAX_MSG_SIZE = 1024
+
+# the second param defines which protocol we are going to use
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+my_socket.sendto("geeks".encode(), (SERVER_IP, PORT))
+(response, remote_address) = my_socket.recvfrom(MAX_MSG_SIZE)
+data = response.decode()
+print("The server sent " + data)
+
+my_socket.close()
